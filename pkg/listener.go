3425e1bf07308435c34fd52db634c03e49a25357
Kishan B <kishancs46@gmail.com>
2022-11-03 13:47:41 +0530

    Add error logs to listener

--- pkg/listener.go
+++ pkg/listener.go
@@ -26,12 +26,19 @@ func NewPostgresRedshiftDataAPIProxy(listenAddress string, simpleQueryFn wire.Si
 }
 
 func (proxy *postgresRedshiftProxy) Run() error {
-	server, err := wire.NewServer(wire.Logger(proxy.logger), wire.SimpleQuery(proxy.simpleQueryFn), wire.ClientAuth(tls.NoClientCert))
+	server, err := wire.NewServer(
+		wire.Logger(proxy.logger),
+		wire.SimpleQuery(proxy.simpleQueryFn),
+		wire.ClientAuth(tls.NoClientCert))
 	if err != nil {
+		proxy.logger.Error("error while instantiating server", zap.Error(err))
 		return fmt.Errorf("error while instantiating server: %w", err)
 	}
 	err = server.ListenAndServe(proxy.listenAddress)
 	if err != nil {
+		proxy.logger.Error("error while listening to listen address",
+			zap.String("listenAddress", proxy.listenAddress),
+			zap.Error(err))
 		return fmt.Errorf("error while listening to %s: %w", proxy.listenAddress, err)
 	}
 	return nil
