99f2510a2c6f3fcb2103eb01d8a397870d9ea5c0
Kishan B <kishancs46@gmail.com>
2022-12-18 13:51:35 +0530

    Add test for args substitution

--- pkg/pg_redshift_translator_test.go
+++ pkg/pg_redshift_translator_test.go
@@ -1,6 +1,11 @@
 package rdapp
 
-import "testing"
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/redshiftdata/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func Test_pgRedshiftTranslator_TranslateToRedshiftQuery(t *testing.T) {
 	type args struct {
@@ -11,15 +16,51 @@ func Test_pgRedshiftTranslator_TranslateToRedshiftQuery(t *testing.T) {
 		args args
 		want string
 	}{
-		{"question mark params", args{query: "select * from person where name = ? and age > ?"}, "select * from person where name = :1 and age > :2"},
-		{"dollar params", args{query: "select * from person where name = $1 and age > $2"}, "select * from person where name = :1 and age > :2"},
+		{
+			name: "question mark params",
+			args: args{query: "select * from person where name = ? and age > ?"},
+			want: "select * from person where name = :1 and age > :2",
+		},
+		{
+			name: "dollar params",
+			args: args{query: "select * from person where name = $1 and age > $2"},
+			want: "select * from person where name = :1 and age > :2",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			translator := &pgRedshiftTranslator{}
-			if got := translator.TranslateToRedshiftQuery(tt.args.query); got != tt.want {
-				t.Errorf("TranslateToRedshiftQuery() = %v, want %v", got, tt.want)
-			}
+			got := translator.TranslateToRedshiftQuery(tt.args.query)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_pgRedshiftTranslator_TranslateToRedshiftQueryParams(t *testing.T) {
+	type args struct {
+		pgParams []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []types.SqlParameter
+	}{
+		{
+			name: "translate args to sql position params",
+			args: args{
+				pgParams: []string{"name", "20"},
+			},
+			want: []types.SqlParameter{
+				{Name: aws.String("1"), Value: aws.String("name")},
+				{Name: aws.String("2"), Value: aws.String("20")},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			translator := &pgRedshiftTranslator{}
+			got := translator.TranslateToRedshiftQueryParams(tt.args.pgParams)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
